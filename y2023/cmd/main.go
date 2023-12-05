70f4b3f01f2ec765f2f8dd92e8e4f7710a5cc159
Corey Gair <corey.gair@nextdlp.com>
2023-12-05 11:22:20 +0000

    fix main

--- y2023/cmd/main.go
+++ y2023/cmd/main.go
@@ -6,13 +6,15 @@ import (
 	"github.com/CoreyGair/aoc/y2023"
 )
 
+type AOCSolution = func(string) int
+
 var (
-	days = [][]func(string) int{
-		[y2023.Day1],
-		[y2023.Day2,y2023.Day2Part2],
-		[y2023.Day3],
-		[y2023.Day4],
-		[y2023.Day5],
+	days = [][]AOCSolution{
+		{y2023.Day1},
+		{y2023.Day2, y2023.Day2Part2},
+		{y2023.Day3},
+		{y2023.Day4},
+		{y2023.Day5},
 	}
 )
 
